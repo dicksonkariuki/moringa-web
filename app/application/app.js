0deb1e08fa056cce37ca56dda67bffa1b563eb1c
vstluan <luizantonio.ccf@gmail.com>
2016-07-12 15:05:06 -0300

    agora a url ta pretty

--- app/application/app.js
+++ app/application/app.js
@@ -1,7 +1,7 @@
 // app.js
 var moringaApp = angular.module('moringaApp', ['ui.router']);
 
-moringaApp.config(function($stateProvider, $urlRouterProvider) {
+moringaApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $urlRouterProvider.otherwise('/inicio');
 
@@ -10,15 +10,16 @@ moringaApp.config(function($stateProvider, $urlRouterProvider) {
     // HOME STATES AND NESTED VIEWS ========================================
         .state('inicio', {
             url: '/inicio',
-            templateUrl: 'app/components/inicio/inicio.html'
+            templateUrl: 'moringa-web/app/components/inicio/inicio.html'
         })
         .state('acude', {
             url: '/acude',
-            templateUrl: 'app/components/acude/acude.html'
+            templateUrl: 'moringa-web/app/components/acude/acude.html'
         })
         .state('sobre', {
         url: '/sobre',
-        templateUrl: 'app/components/sobre/sobre.html'
+        templateUrl: 'moringa-web/app/components/sobre/sobre.html'
     });
 
+    $locationProvider.html5Mode(true)
 });
