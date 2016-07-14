d54e992b2c35936db14a07a2a25af8b370ffb85e
Jordao Serafim <jordaoesa@gmail.com>
2016-07-14 11:11:19 -0300

    adicionando node/express no projeto sem modificar a hierarquia de arquivos

--- app/application/properties.js
+++ app/application/properties.js
@@ -1,5 +1,6 @@
 var Properties = {
-     webserviceAddress: 'http://150.165.98.43:8080'
+    webserviceAddress: 'http://moringa-webservice.herokuapp.com'
+    // webserviceAddress: 'http://150.165.98.43:8080'
     // webserviceAddress: 'http://192.168.1.104:8080'
     // webserviceAddress: 'http://localhost:8080'
 };
